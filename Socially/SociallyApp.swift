75664ab1724690b30c3506b473a1a4d65f1c0c2d
Robert Sandu <rsandu1@fordham.edu>
2023-05-12 12:50:11 -0400

    Initial Commit

---
+++ Socially/SociallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SociallyApp.swift
+//  Socially
+//
+//  Created by Robert Sandu on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SociallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
